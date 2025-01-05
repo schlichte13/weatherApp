9ff15d3a2fcc9479791f5e13b12eab2b8ef066de
DESKTOP-KSE9D76\humpt <alstonstone0@gmail.com>
2025-01-05 18:03:55 -0500

    Line for updatiang the local forecast.
    a value must be entered to get the zip code. For now defaults to concord always.
    
    TODO, zip cod to long and lat values

--- weatherApp.js
+++ weatherApp.js
@@ -1,6 +1,28 @@
+
+const DEFAULT_WEATHER_API_LINK ='https://api.weather.gov/gridpoints/GYX/';
+var WEATHER_API_LINK = 'https://api.weather.gov/gridpoints/GYX/38,31/forecast';
+
 let weatherData = [];
 
-fetch('https://api.weather.gov/gridpoints/GYX/38,31/forecast')
+
+const getForecastButton = document.getElementById("getForecastButton");
+getForecastButton.addEventListener("click", () => {
+    localizeWeatherAPI();
+})
+
+function localizeWeatherAPI(){
+    let inputZipCode = document.querySelector("#zipCode").value
+    if(inputZipCode !== ""){
+        setLocalizedApi(38,31);
+        updateContent();
+    }
+}
+
+function setLocalizedApi(longitude, latitude){
+    WEATHER_API_LINK = DEFAULT_WEATHER_API_LINK+longitude+','+latitude+'/forecast';
+}
+function updateContent(){
+    fetch(WEATHER_API_LINK)
     .then(response => {
         if (response.ok){
             return response.json(); //Parse the response data as JSON
@@ -12,13 +34,11 @@ fetch('https://api.weather.gov/gridpoints/GYX/38,31/forecast')
         //Process the response data here
         //Use data according to your need
         weatherData = data.properties.periods[0].detailedForecast; //Example: Logging the data to the console
-        document.getElementById('output').textContent = weatherData;
-
+        document.getElementById('weatherForecastText').textContent = weatherData;
         //JSON.filter?
     })
     .catch(error => {
         //Handle any errors here
         console.error(error); //Example: Logging the error to the console
     });
-    
-
+}
